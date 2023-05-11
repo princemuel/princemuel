205ca1d50287e0d95300a24056cfc163d6f4df5a
Prince Muel <vansomecsam@gmail.com>
2023-05-11 18:26:00 +0100

    ci: update next config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+    webVitalsAttribution: ['CLS', 'LCP'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
