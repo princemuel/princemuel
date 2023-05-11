ad1c2e21c2f00f6b5adf8ff908eb129c0356b0c0
Prince Muel <vansomecsam@gmail.com>
2023-05-11 20:11:16 +0100

    feat: add project component types
    Reference-to:
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- project.d.ts
+++ project.d.ts
@@ -1,3 +1,11 @@
 interface IUser {
   name: string;
 }
+
+type $ElementProps<E extends React.ElementType<any>> = {
+  children: React.ReactNode;
+  as?: E;
+};
+
+type ElementProps<E extends React.ElementType<any>> = $ElementProps<E> &
+  Omit<React.ComponentPropsWithoutRef<E>, keyof $ElementProps<E>>;
