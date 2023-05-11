d10c39a8700447895933f593b255526ffb0c6567
Prince Muel <vansomecsam@gmail.com>
2023-05-11 19:37:22 +0100

    feat: setup page routes
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-export default function Home() {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Prince Muel',
+  description: '',
+};
+
+interface Props {}
+
+const Page = (props: Props) => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
   );
-}
+};
+
+export default Page;
---
+++ app/projects/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {};
+
+interface Props {}
+
+const Page = (props: Props) => {
+  return <div>Page</div>;
+};
+
+export default Page;
