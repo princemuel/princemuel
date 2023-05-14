95bf51e1cf0b960d60bfde25d2798f4e796a98ab
Prince Muel <vansomecsam@gmail.com>
2023-05-14 21:02:04 +0100

    ci: update nextjs config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+// const withMDX = require('@next/mdx')();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   experimental: {
     typedRoutes: true,
     webVitalsAttribution: ['CLS', 'LCP'],
+  },
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
   },
 };
 
