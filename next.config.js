87202be2477f55d40ffc5f68dfd69b8b5a80641f
Prince Muel <vansomecsam@gmail.com>
2023-05-15 21:13:26 +0100

    ci: update nextjs config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
   devIndicators: {
     buildActivityPosition: 'bottom-right',
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
