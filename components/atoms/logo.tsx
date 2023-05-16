eff27c32da5f93dfb426c7f562df760ee27242bc
Prince Muel <vansomecsam@gmail.com>
2023-05-16 02:58:19 +0100

    feat: add styles for logo svg
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/logo.tsx
+++ components/atoms/logo.tsx
@@ -11,7 +11,10 @@ const Logo = ({ className }: Props) => {
     <Link href={'/'} aria-label='Go to Home' legacyBehavior>
       <a className={clsx(className)}>
         <span className='sr-only'>Go to Home</span>
-        <icons.logo aria-hidden='true' className='fill-current' />
+        <icons.logo
+          aria-hidden='true'
+          className='fill-current stroke-transparent'
+        />
       </a>
     </Link>
   );
