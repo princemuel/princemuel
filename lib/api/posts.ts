f7d02e1a928fac7a3961448d1a1165c397a3dcfa
Prince Muel <vansomecsam@gmail.com>
2023-05-17 00:57:43 +0100

    perf: add improvements to data fetching
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- lib/api/posts.ts
+++ lib/api/posts.ts
@@ -1,63 +1,69 @@
 import { Player } from '@/components';
 import { compileMDX } from 'next-mdx-remote/rsc';
+import { cache } from 'react';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings/lib';
 import rehypeHighlight from 'rehype-highlight/lib';
 import rehypeSlug from 'rehype-slug';
+import 'server-only';
 import { REPO_PATH } from './constants';
 import { getRepoFiletree } from './filetree';
 
-export const getPostByName = async (
-  fileName: string
-): Promise<IPost | undefined> => {
-  const res = await fetch(
-    `https://raw.githubusercontent.com/${REPO_PATH}/main/posts/${fileName}`,
-    {
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    }
-  );
-
-  if (!res.ok) return undefined;
-
-  const data = await res.text();
-  if (data === '404: Not Found') return undefined;
-
-  const { frontmatter, content } = await compileMDX<IPostMeta>({
-    source: data,
-    components: {
-      Player,
-    },
-    options: {
-      parseFrontmatter: true,
-      mdxOptions: {
-        rehypePlugins: [
-          rehypeHighlight,
-          rehypeSlug,
-          [
-            rehypeAutolinkHeadings,
-            {
-              behavior: 'wrap',
-            },
-          ],
-        ],
-      },
-    },
-  });
-
-  const id = fileName.replace(/\.mdx$/, '');
-  return {
-    meta: {
-      id,
-      title: frontmatter.title,
-      date: frontmatter.date,
-      tags: frontmatter.tags,
-    },
-    content,
-  } satisfies IPost;
+export const preload = (fileName: string) => {
+  void getPostByName(fileName);
 };
+
+export const getPostByName = cache(
+  async (fileName: string): Promise<IPost | undefined> => {
+    const res = await fetch(
+      `https://raw.githubusercontent.com/${REPO_PATH}/main/posts/${fileName}`,
+      {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }
+    );
+
+    if (!res.ok) return undefined;
+
+    const data = await res.text();
+    if (data === '404: Not Found') return undefined;
+
+    const { frontmatter, content } = await compileMDX<IPostMeta>({
+      source: data,
+      components: {
+        Player,
+      },
+      options: {
+        parseFrontmatter: true,
+        mdxOptions: {
+          rehypePlugins: [
+            rehypeHighlight,
+            rehypeSlug,
+            [
+              rehypeAutolinkHeadings,
+              {
+                behavior: 'wrap',
+              },
+            ],
+          ],
+        },
+      },
+    });
+
+    const id = fileName.replace(/\.mdx$/, '');
+    return {
+      meta: {
+        id,
+        title: frontmatter.title,
+        date: frontmatter.date,
+        tags: frontmatter.tags,
+      },
+      content,
+    } satisfies IPost;
+  }
+);
 
 export async function getPostsMetadata() {
   const filetrees = await getRepoFiletree();
