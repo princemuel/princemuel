705deb64f3ba8a86bc66f0735dd3e830eeeaec4c
Prince Muel <vansomecsam@gmail.com>
2023-05-16 01:40:19 +0100

    feat: add logo component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/atoms/logo.tsx
@@ -0,0 +1,20 @@
+import { icons } from '@/common';
+import clsx from 'clsx';
+import Link from 'next/link';
+
+interface Props {
+  className?: string;
+}
+
+const Logo = ({ className }: Props) => {
+  return (
+    <Link href={'/'} aria-label='Go to Home' legacyBehavior>
+      <a className={clsx(className)}>
+        <span className='sr-only'>Go to Home</span>
+        <icons.logo aria-hidden='true' className='fill-current' />
+      </a>
+    </Link>
+  );
+};
+
+export { Logo };
