95c103d6966f8182183006d2c45d5fc525b71161
Prince Muel <vansomecsam@gmail.com>
2023-05-16 01:50:24 +0100

    feat: add project type definition files
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- @types/project.d.ts
+++ @types/project.d.ts
@@ -2,6 +2,12 @@ interface IUser {
   name: string;
 }
 
+type ExtractElementProps<T> = T extends React.ComponentType<infer Props>
+  ? Props extends object
+    ? Props
+    : never
+  : never;
+
 type $ElementProps<E extends React.ElementType<any>> = {
   children: React.ReactNode;
   as?: E;
@@ -9,3 +15,5 @@ type $ElementProps<E extends React.ElementType<any>> = {
 
 type ElementProps<E extends React.ElementType<any>> = $ElementProps<E> &
   Omit<React.ComponentPropsWithoutRef<E>, keyof $ElementProps<E>>;
+
+interface IconProps extends React.ComponentPropsWithoutRef<'svg'> {}
