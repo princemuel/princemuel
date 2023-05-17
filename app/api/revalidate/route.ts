3e2a3994b420944a34f1b6a69a5d9f2065a34275
Prince Muel <vansomecsam@gmail.com>
2023-05-18 00:22:35 +0100

    fix: partial resolve to buikld issue
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
-import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
+  return NextResponse.json({ message: 'Hello World' });
   // const secret = request.nextUrl.searchParams.get('secret');
 
   // if (secret !== process.env.REVALIDATE_SECRET) {
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
   //   );
   // }
 
-  const path = request.nextUrl.searchParams.get('path') || '/';
-  revalidatePath(path);
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  // const path = request.nextUrl.searchParams.get('path') || '/';
+  // revalidatePath(path);
+  // return NextResponse.json({ revalidated: true, now: Date.now() });
 }
