e33ec7c8ccce41cbbee146940ea07d86552dd5ca
Prince Muel <vansomecsam@gmail.com>
2023-05-21 12:00:11 +0100

    feat: setup page navigation
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/organisms/header.tsx
+++ components/organisms/header.tsx
@@ -1,21 +1,11 @@
-import clsx from 'clsx';
-import { Logo } from '../atoms';
 import { Navigation } from '../molecules';
 
 interface Props {}
 
 export function Header(props: Props) {
   return (
-    <header className='bg-white text-white'>
-      <div className='full-width-shadow h-container'>
-        <div className={clsx('relative py-10 text-zinc-800')}>
-          <div>
-            <Logo className='text-black transition-all delay-0 duration-300  ease-in hover:text-teal-500 focus:text-teal-500 active:text-teal-500' />
-          </div>
-
-          <Navigation />
-        </div>
-      </div>
+    <header className='sticky top-0 z-50 bg-white text-white'>
+      <Navigation />
     </header>
   );
 }
