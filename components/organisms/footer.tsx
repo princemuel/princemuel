999ea586a8d967d89d820419a8276083563afdbc
Prince Muel <vansomecsam@gmail.com>
2023-05-21 12:07:11 +0100

    feat: update footer styles
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/organisms/footer.tsx
+++ components/organisms/footer.tsx
@@ -1,13 +1,13 @@
 import { links } from '@/common';
 import clsx from 'clsx';
 import { Logo, NavLink, SocialIcon, Text } from '../atoms';
-import styles from './styles.module.css';
+import styles from './styles/footer.module.css';
 
 interface Props {}
 
 export function Footer(props: Props) {
   return (
-    <footer className='mt-40 bg-zinc-800 full-width-shadow h-container'>
+    <footer className='mt-40 bg-zinc-800 text-zinc-800 full-width-shadow h-container'>
       <section className="relative py-32 text-white/75 before:absolute before:left-2/4 before:top-0 before:h-2 before:w-40 before:-translate-x-2/4 before:bg-teal-500 before:content-[''] before:md:left-0 before:md:translate-x-0">
         <div className='mb-12 flex flex-col items-center gap-12 text-center md:items-start md:text-left lg:flex-row lg:justify-between'>
           <Logo className='text-white transition-all delay-0 duration-300  ease-in hover:text-teal-500 focus:text-teal-500 active:text-teal-500' />
