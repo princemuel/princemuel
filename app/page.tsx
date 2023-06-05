98ae4010ab108576275c5fb1b98477f4ab416d5d
Prince Muel <vansomecsam@gmail.com>
2023-06-05 20:44:56 +0100

    feat: update home page route
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import { Text } from '@/components';
-import { getProjectsMetadata, hasValues } from '@/lib';
+import { HomeTemplate } from '@/components';
 import { Metadata } from 'next';
-import * as React from 'react';
 
 export const metadata: Metadata = {
   title: 'Prince Muel',
@@ -9,25 +7,16 @@ export const metadata: Metadata = {
 };
 
 const PageRoute = async () => {
-  const projects = await getProjectsMetadata();
-
   return (
-    <main className=''>
-      {/* HERO SECTION */}
-
-      {/*  */}
-      <h1>This page is still in development. It should be ready soon </h1>
-
-      <React.Fragment>
-        {!hasValues(projects) ? (
-          <Text className='mt-10 text-center'>
-            Sorry, no projects are available yet.
-          </Text>
-        ) : (
-          <section>{JSON.stringify(projects, null, 2)}</section>
-        )}
-      </React.Fragment>
-    </main>
+    <>
+      <main
+        id='main-content'
+        aria-label='Contact Page'
+        className='flex flex-col gap-20 !bg-white'
+      >
+        <HomeTemplate />
+      </main>
+    </>
   );
 };
 
