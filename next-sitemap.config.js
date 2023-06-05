3fdc9faa0bd2a360d5b74edf6fe9787522fb7038
Prince Muel <vansomecsam@gmail.com>
2023-06-05 16:13:43 +0100

    ci: update sitemap config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://your-domain.com',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
     additionalSitemaps: [`${process.env.SITE_URL}/server-sitemap.xml`],
