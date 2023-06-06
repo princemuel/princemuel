92a99c3a9ac90f6b375af1412ba7ed1ca4f67ace
Prince Muel <vansomecsam@gmail.com>
2023-06-06 15:43:02 +0100

    feat: update fonts and robots txt generator
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,13 +1,6 @@
-import { Ibarra_Real_Nova, Public_Sans } from 'next/font/google';
-import './index.css';
+import { Inter } from 'next/font/google';
 
-export const ibarraRealNova = Ibarra_Real_Nova({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-serif',
-});
-
-export const publicSans = Public_Sans({
+export const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-sans',
