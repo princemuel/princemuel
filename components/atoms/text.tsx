3d67899836cee8634e4e37bcd2079364975c8682
Prince Muel <vansomecsam@gmail.com>
2023-07-03 17:00:01 +0100

    perf: update text component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/text.tsx
+++ components/atoms/text.tsx
@@ -1,10 +1,66 @@
+import { cn } from '@/lib';
+import type { VariantProps } from 'cva';
+import { cva } from 'cva';
+
+const textVariants = cva('', {
+  defaultVariants: {
+    intent: 'primary',
+    size: 's',
+  },
+  variants: {
+    intent: {
+      primary: 'text-brand-900 dark:text-white',
+      secondary: 'text-brand-400 dark:text-brand-400',
+      outline: '',
+    },
+    size: {
+      s: '',
+      xs: 'text-300 leading-200',
+      sm: 'text-400 leading-400',
+      md: 'text-500 leading-300',
+      lg: 'text-600 leading-400',
+      xl: 'text-700 leading-500',
+    },
+
+    weight: {
+      bold: 'font-bold',
+      medium: 'font-medium',
+      regular: 'font-normal',
+    },
+  },
+  compoundVariants: [
+    {
+      intent: 'secondary',
+      size: 'xs',
+      weight: 'bold',
+      class: 'tracking-100',
+    },
+  ],
+});
+
+interface TextVariants extends VariantProps<typeof textVariants> {}
+const text = (variants: TextVariants, className = '') =>
+  cn(textVariants(variants), className);
+// const text = (variants: TextVariants) => textVariants(variants);
+
+type Props<E extends React.ElementType = 'p'> = ElementProps<E> & TextVariants;
+
 const Text = <E extends React.ElementType = 'p'>({
   as,
+  intent,
+  weight,
+  size,
+  className,
   children,
   ...rest
-}: ElementProps<E>) => {
+}: Props<E>) => {
   const Rendered = as || 'p';
-  return <Rendered {...rest}>{children}</Rendered>;
+
+  return (
+    <Rendered className={text({ intent, weight, size }, className)} {...rest}>
+      {children}
+    </Rendered>
+  );
 };
 
 export { Text };
