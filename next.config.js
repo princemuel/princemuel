f9be69fbbecdd2349736d66b404aeefb36e5dffe
Prince Muel <vansomecsam@gmail.com>
2023-07-03 17:25:00 +0100

    ci: update tailwind and next  and prettier configs
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,18 +2,61 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   reactStrictMode: true,
   experimental: {
     typedRoutes: true,
     webVitalsAttribution: ['CLS', 'LCP'],
+    // turbo: {
+    //   loaders: { '.svg': ['@svgr/webpack'] },
+    // },
   },
-  devIndicators: {
-    buildActivityPosition: 'bottom-right',
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /(?<!inline)\.svg$/i,
+      },
+      {
+        test: /\.inline.svg$/i,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgo: true,
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                  'prefixIds',
+                ],
+              },
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    return config;
   },
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,46 +10,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    aspectRatio: {
-      auto: 'auto',
-      square: '1 / 1',
-      video: '16 / 9',
-      1: '1',
-      2: '2',
-      3: '3',
-      4: '4',
-      5: '5',
-      6: '6',
-      7: '7',
-      8: '8',
-      9: '9',
-      10: '10',
-      11: '11',
-      12: '12',
-      13: '13',
-      14: '14',
-      15: '15',
-      16: '16',
-    },
     borderRadius: {
       ...defaultTheme.borderRadius,
       pill: '100vmax',
     },
 
-    // screens: {
-    //   s: "20em", // => @media (min-width: 320px) { ... }
-    //   xs: "30em", // => @media (min-width: 480px) { ... }
-    //   sm: "36em", // => @media (min-width: 576px) { ... }
-    //   sx: "40em", // => @media (min-width: 640px) { ... }
-    //   md: "48em", // => @media (min-width: 768px) { ... }
-    //   lg: "64em", // => @media (min-width: 1024px) { ... }
-    //   xl: "80em", // => @media (min-width: 1280px) { ... }
-    //   xxl: "96em", // => @media (min-width: 1536px) { ... }
-    //   xxxl: "112.5em", // => @media (min-width: 1800px) { ... }
-    // },
-
     screens: {
-      xs: '30em', // => @media (min-width: 480px) { ... }
+      sx: '20em', // => @media (min-width: 320px) { ... }
+      s: '30em', // => @media (min-width: 480px) { ... }
       ...defaultTheme.screens,
     },
 
@@ -59,14 +27,17 @@ module.exports = {
           'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAUCAYAAABvVQZ0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAI6SURBVHgBnZRBaxNBGIYnu9uNSW1NlkgMVAl4Cyg5CAUPJlYFTyaX3CohePKUn5BcBA9CfkAvAf+B4sVLBC8eChv05ClKVaQIiUKjabOO77s7E8bUBpsXnrwzu9lvvvm+2bXFccWADRw1XlpWJpNZg+8ACV4kEomNVqtliSW0Am2mUik5HA5luVxmwEe5XC4pltBZ27a38/m8pHq9HoP54KI47ZbT6fQ52H1mRjE7jnFtK5vNrjJzhSuiui5cYA2Z3YN/HwwGYcBischgH8En8BLsKZ5w8VqtdiwoJ5bneeuO49zGeBdFD4OVSqVwu77vh1nS9VgF3SoUCq4ZiJObzAgcgK8sPtVsNqWWvka1221d011wgckI9cPJ5263K7k9NkDXbZH4Xzz3DRRVLcPDycmYBTe6KPV8kUR0Hm+AM0JFvA4+6DpRnU5nNq5UKn85A5jBUOdb8ITO7CrY4dZ0F+dX/5cbmd0Fq6zXb7CPI/F6NBq9bTQa4n/V7/dpI2QmZxfZWugKhm3eNLdI6S5q1/VkozB+BjZB3Dwe6+COZVlP4Ufs7CLV63UGeg8egksieiNmspPJZA6+DZ6DX/MZVqvVMDt1JA6w8GP4NV38ebnxePwy/IEK+FMVWHMIhmr8BnWuwj2hDqwzF+xwMpnsIeCr6XQ6DoJgIKIvhhuLxaZIjm/IEZgAH/ffwX+IqIknvvU8e+fBBlb38JALDxBwrLKTWGwf/gWMzcKfJP3pXjGuBcqlyjAwH/gDZjDKatJ5fJYAAAAASUVORK5CYII="), pointer;',
       },
       screens: {
+        sx: '36em', // => @media (min-width: 576px) { ... },
         sm: '40em', // => @media (min-width: 640px) { ... }
         md: '48em', // => @media (min-width: 768px) { ... }
         lg: '64em', // => @media (min-width: 1024px) { ... }
         xl: '80em', // => @media (min-width: 1280px) { ... }
         '2xl': '96em', // => @media (min-width: 1536px) { ... }
+        '3xl': '112.5em', // => @media (min-width: 1800px) { ... }
       },
       fontFamily: {
         sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -81,15 +52,20 @@ module.exports = {
     require('tailwind-scrollbar')({ nocompatible: true }),
     plugin(function ({ addUtilities }) {
       addUtilities({
-        '.full-width-shadow': {
+        '.full-w-bg': {
           boxShadow: '0 0 0 100vmax currentColor, 0 0 2rem currentColor',
           clipPath: 'inset(0 -100vmax)',
         },
         '.h-container': {
-          '--max-width': '111rem',
+          '--max-width': '112rem',
           '--container-padding': '1.6rem',
           width: 'min(var(--max-width), 100% - (var(--container-padding) * 2))',
           marginInline: 'auto',
+        },
+        '.grid-auto': {
+          '--min-column-size': '22rem',
+          gridTemplateColumns:
+            'repeat(auto-fit, minmax(min(var(--min-column-size), 100%), 1fr))',
         },
       });
     }),
