8e16df06199a53d6dc02a67cf742e11a0e111c89
Prince Muel <vansomecsam@gmail.com>
2023-07-04 07:24:47 +0100

    fix: resolve build issue
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
-import { ContactTemplate } from '@/components';
 import { Metadata } from 'next';
+import { ContactTemplate } from './contact';
 
 export const metadata: Metadata = {
   title: 'Contact Page',
