2964f97683f53673076cbbce2b24058a1abbf418
Prince Muel <vansomecsam@gmail.com>
2023-07-26 03:16:39 +0100

    feat: add skeleton card component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/responsive-video.tsx
+++ components/atoms/responsive-video.tsx
@@ -1,10 +1,12 @@
 export function ResponsiveVideo({ slug }: IParams) {
   return (
-    <div className='aspect-w-16 aspect-h-9'>
+    <div className='container relative overflow-auto rounded-xl p-8'>
       <iframe
+        className='aspect-video w-full rounded-lg shadow-lg'
         src={`https://www.youtube.com/embed/${slug}`}
         title='YouTube video player'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowFullScreen={false}
       />
     </div>
   );
---
+++ components/atoms/underline.tsx
@@ -0,0 +1,3 @@
+export const NavlinkUnderline = () => (
+  <span className="absolute inset-x-1 -bottom-px hidden h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 group-aria-[current='page']:inline dark:from-teal-400 dark:via-teal-400/40 dark:to-teal-400/0" />
+);
