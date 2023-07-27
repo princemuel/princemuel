4e1565e92e746743d75df003b21f80109eafcad3
Prince Muel <vansomecsam@gmail.com>
2023-07-27 05:45:10 +0100

    perf: improve theme switch
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/theme.tsx
+++ components/atoms/theme.tsx
@@ -2,13 +2,31 @@
 
 import { cn } from '@/lib';
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
-import { Laptop, MoonIcon, Sun } from 'lucide-react';
+import { Laptop2, MoonStar, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { startTransition, useCallback } from 'react';
 
 interface Props {
   className?: string;
 }
+
+const options = [
+  {
+    value: 'light',
+    label: 'Light theme',
+    icon: Sun,
+  },
+  {
+    value: 'system',
+    label: 'System theme',
+    icon: Laptop2,
+  },
+  {
+    value: 'dark',
+    label: 'Dark theme',
+    icon: MoonStar,
+  },
+];
 
 export const ThemeSwitch = ({ className }: Props) => {
   const { theme, setTheme } = useTheme();
@@ -23,6 +41,7 @@ export const ThemeSwitch = ({ className }: Props) => {
 
   return (
     <ToggleGroup.Root
+      id='theme-switch'
       type='single'
       aria-label='Theme Mode Switch'
       className={cn('flex items-center gap-3 p-1', className)}
@@ -31,30 +50,16 @@ export const ThemeSwitch = ({ className }: Props) => {
         if (value) updateTheme(value);
       }}
     >
-      <ToggleGroup.Item
-        value='light'
-        aria-label='Choose light theme'
-        className={cn('')}
-      >
-        <Sun strokeWidth={1} />
-        <span className='sr-only'>Select light theme</span>
-      </ToggleGroup.Item>
-
-      <ToggleGroup.Item
-        value='system'
-        aria-label='Use default system theme'
-        className={cn('')}
-      >
-        <Laptop strokeWidth={1} />
-      </ToggleGroup.Item>
-
-      <ToggleGroup.Item
-        value='dark'
-        aria-label='Choose dark theme'
-        className={cn('')}
-      >
-        <MoonIcon strokeWidth={1} />
-      </ToggleGroup.Item>
+      {options.map(({ value, label, icon: Icon }) => (
+        <ToggleGroup.Item
+          key={value}
+          value={value}
+          aria-label={label}
+          className={cn('')}
+        >
+          <Icon strokeWidth={1} />
+        </ToggleGroup.Item>
+      ))}
     </ToggleGroup.Root>
   );
 };
