b97dcf972e224fa455c31136219c5ae78cdbaa16
Prince Muel <vansomecsam@gmail.com>
2023-08-03 07:50:49 +0100

    feat: add svg asset
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- common/assets.ts
+++ common/assets.ts
@@ -1,7 +1,15 @@
+import GrainyBackgroundSVG from '@/assets/icons/grainy.inline.svg';
 import IconFacebook from '@/assets/icons/icon-facebook.inline.svg';
 import IconGitHub from '@/assets/icons/icon-github.inline.svg';
 import IconInstagram from '@/assets/icons/icon-instagram.inline.svg';
 import IconLinkedIn from '@/assets/icons/icon-linkedin.inline.svg';
 import IconTwitter from '@/assets/icons/icon-twitter.inline.svg';
 
-export { IconFacebook, IconGitHub, IconInstagram, IconLinkedIn, IconTwitter };
+export {
+  GrainyBackgroundSVG,
+  IconFacebook,
+  IconGitHub,
+  IconInstagram,
+  IconLinkedIn,
+  IconTwitter,
+};
