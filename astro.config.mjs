d1c807abadca17e70ccf795fc387b0d34297bf51
Prince Muel <vansomecsam@gmail.com>
2023-12-07 00:05:45 +0100

    deps: add vercel og for og image generation
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,34 @@
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.VERCEL_URL,
+  output: "hybrid",
+  adapter: vercel({
+    functionPerRoute: true,
+    imageService: true,
+  }),
   integrations: [
-    tailwind(),
-    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
     react({
       include: ["**/react/*"],
     }),
+    mdx(),
+    sitemap({ changefreq: "daily", priority: 0.7 }),
   ],
+  redirects: {
+    "/twitter": "https://x.com/iamprincemuel",
+    "/linkedin": "https://linkedin.com/in/iamprincemuel",
+    "/youtube": "https://youtube.com/@princemuel",
+    "/github": "https://github.com/princemuel",
+    "/sponsors": "https://github.com/sponsors/princemuel",
+    "/coffee": "",
+  },
 });
