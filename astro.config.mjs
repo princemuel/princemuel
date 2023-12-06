0e65aa42d40e9edd0e4a23290db881c3a52b58ad
Prince Muel <vansomecsam@gmail.com>
2023-12-06 06:14:21 +0100

    ci: setup astro config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    mdx(),
+    react({
+      include: ["**/react/*"],
+    }),
+  ],
+});
