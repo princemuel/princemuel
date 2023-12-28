da6180c8d87d9a4d9bcf4ce2a32ed78d1dfc6bf1
Prince Muel <vansomecsam@gmail.com>
2023-12-28 04:16:46 +0100

    feat: add project reset and helper types
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/@types/project..d.ts
@@ -0,0 +1,5 @@
+type RequireSome<T, P extends keyof T> = Omit<T, P> & Required<Pick<T, P>>;
+
+type MapKeys<M extends Map<unknown, unknown>> = Prettify<Array<Parameters<M["get"]>[0]>>;
+
+type Prettify<T> = { [K in keyof T]: T[K] } & {};
