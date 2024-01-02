90f7c10e0cf62c8dd31fd93fe145b51da766dc42
Prince Muel <vansomecsam@gmail.com>
2024-01-02 02:12:00 +0100

    ci: update project types
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/@types/project..d.ts
+++ src/@types/project..d.ts
@@ -1,5 +1,5 @@
+type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
 type RequireSome<T, P extends keyof T> = Omit<T, P> & Required<Pick<T, P>>;
 
 type MapKeys<M extends Map<unknown, unknown>> = Prettify<Array<Parameters<M["get"]>[0]>>;
-
-type Prettify<T> = { [K in keyof T]: T[K] } & {};
