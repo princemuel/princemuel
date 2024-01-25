a5255c2f99a7e5d53f2f13dc630e783911ac158d
Prince Muel <vansomecsam@gmail.com>
2024-01-25 16:35:24 +0100

    ci: update eslint config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,18 @@ module.exports = {
     node: true,
     es2022: true,
     browser: true,
+    worker: true,
+    "shared-node-browser": true,
   },
   extends: ["eslint:recommended", "plugin:astro/recommended", "plugin:astro/jsx-a11y-strict"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
   },
   rules: {
     "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
@@ -23,53 +30,44 @@ module.exports = {
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
+      extends: ["plugin:astro/recommended", "plugin:astro/jsx-a11y-strict"],
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
-      rules: {},
+      rules: {
+        "no-undef": "off",
+        "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      },
     },
     {
-      files: ["*.ts"],
+      files: ["*.d.ts"],
       parser: "@typescript-eslint/parser",
       extends: [
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
-      rules: {
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
-        ],
-        "@typescript-eslint/no-non-null-assertion": "off",
-      },
-    },
-    {
-      files: ["*.d.ts"],
       rules: {
         "@typescript-eslint/triple-slash-reference": "off",
       },
     },
     {
       files: ["*.tsx"],
-      parser: "@typescript-eslint/parser",
       plugins: ["react", "@typescript-eslint"],
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json"],
-      },
+      extends: ["plugin:react/recommended", "plugin:react/jsx-runtime"],
+    },
+    {
+      files: ["*.ts", "*.mts", "*.cts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
       extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
       rules: {
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
-        ],
+        "no-undef": "off",
+        // "@typescript-eslint/restrict-template-expressions": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
         "@typescript-eslint/no-non-null-assertion": "off",
       },
     },
