4fbbf9676eec3b8a9623f90bd754f5e0400b72f3
Prince Muel <vansomecsam@gmail.com>
2024-01-25 16:37:28 +0100

    feat: add github metadata
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/@types/project.d.ts
+++ src/@types/project.d.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
 type Prettify<T> = { [K in keyof T]: T[K] } & {};
 
 type RequireSome<T, P extends keyof T> = Omit<T, P> & Required<Pick<T, P>>;
