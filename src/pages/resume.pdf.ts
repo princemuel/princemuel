2bb54cfa9ed67c82c92dfaabadb4ea9b08f54df7
Prince Muel <vansomecsam@gmail.com>
2024-02-22 23:32:31 +0100

    feat: add resume resource route
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/pages/resume.pdf.ts
@@ -0,0 +1,29 @@
+import { invariant, NetworkError } from "@/helpers";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => {
+  try {
+    const fileId = import.meta.env.GOOGLE_DRIVE_FILE_ID;
+    const apiKey = import.meta.env.GOOGLE_DRIVE_API_KEY;
+
+    const response = await fetch(
+      `https://www.googleapis.com/drive/v3/files/${fileId}/export?mimeType=application/pdf&key=${apiKey}`,
+    );
+
+    invariant.as(NetworkError, response.ok, "Failed to fetch resource");
+
+    const pdfBuffer = Buffer.from(await response.arrayBuffer());
+
+    return new Response(pdfBuffer, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: {
+        ...response.headers,
+        "Content-Disposition": 'inline; filename="resume.pdf"',
+      },
+    });
+  } catch (error) {
+    console.error("Error exporting PDF:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+};
