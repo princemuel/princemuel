40eca57ad0ccf6df3fee3428e6721f5bf1405090
Prince Muel <vansomecsam@gmail.com>
2024-02-25 13:55:15 +0100

    feat: update components, tsconfig

--- src/components/nav.mobile.tsx
+++ src/components/nav.mobile.tsx
@@ -2,11 +2,11 @@ import { component$ } from "@builder.io/qwik";
 import type { CollectionEntry } from "astro:content";
 
 type Props = {
-  toggle?: React.ReactNode;
-  link?: React.ReactNode;
   routes: CollectionEntry<"routes">[];
 };
 
-export const MobileNavigation = component$((props: Props) => {
-  return <div>{JSON.stringify(props, null, 2)}</div>;
+const MobileNavigation = component$((_props: Props) => {
+  return <div></div>;
 });
+
+export default MobileNavigation;
