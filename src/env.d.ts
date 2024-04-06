c4afd66b31082c7f036a2825ef3240694b57a03e
Prince Muel <vansomecsam@gmail.com>
2024-04-06 04:17:46 +0100

    feat: update project types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,21 +1,9 @@
-/// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
+/// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="simple-stack-form/types" />
 
-interface ImportMetaEnv {
-  readonly GOOGLE_DRIVE_TOKEN: string;
-  readonly GOOGLE_DRIVE_FILE_ID: string;
-
-  readonly RESEND_ADDRESS_FROM: string;
-  readonly RESEND_ADDRESS_TO: string;
-  readonly RESEND_AUDIENCE_ID: string;
-  readonly RESEND_TOKEN: string;
-
-  readonly OCTOKIT_TOKEN: string;
-  readonly ENABLE_RESOURCE_PREVIEW: string;
-  readonly PUBLIC_SITE_URL: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
