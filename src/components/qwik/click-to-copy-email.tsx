da07b62fad0030f71a313a8e20a24089892d108a
Prince Muel <vansomecsam@gmail.com>
2024-04-16 21:58:03 +0100

    feat: add user theme toggle finally!!! 🎉
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/qwik/click-to-copy-email.tsx
+++ src/components/qwik/click-to-copy-email.tsx
@@ -26,7 +26,7 @@ const ClickToCopyEmail = component$(() => {
         fill="none"
         viewBox="0 0 24 24"
         stroke-width="1.5"
-        class="size-4 stroke-tahiti-500"
+        class="stroke-brand-500 size-4"
       >
         <path
           stroke-linecap="round"
--- src/components/qwik/index.ts
+++ src/components/qwik/index.ts
@@ -1,1 +1,2 @@
 export { default as ClickToCopyEmail } from "./click-to-copy-email";
+// export { ThemeSwitch } from "./theme-switch";
