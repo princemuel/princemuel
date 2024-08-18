a0374322ce9a2bee700459da5dbbd9ef147b1039
Prince Muel <vansomecsam@gmail.com>
2024-08-18 23:33:01 +0100

    docs: add documentation files

---
+++ app/pages/feed.xsl.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from "astro";
+
+import feedStyles from "@/assets/styles/xml-feed.css?raw";
+import feedTemplate from "@/assets/styles/xml-feed.xsl?raw";
+
+export const GET: APIRoute = () => {
+  const styles = feedTemplate
+    .replace("<!-- {{ styles }} -->", `<style>\n${feedStyles}\n</style>`)
+    .replaceAll(/\t|\n/giu, "")
+    .replaceAll(/\s+/giu, " ");
+
+  return new Response(styles, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml; charset=UTF-8",
+      "X-Content-Type-Options": "nosniff",
+    },
+  });
+};
