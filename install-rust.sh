9446080ef1fb067dc19bb826b980eeea54a37c7a
Prince Muel <vansomecsam@gmail.com>
2024-08-19 00:21:00 +0100

    ci: update build scrict permissions

--- install-rust.sh
+++ install-rust.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Install Rust using rustup (non-interactive, auto-confirm)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+
+# Source the Rust environment to make Cargo available
+. "$HOME/.cargo/env"
+
+# Confirm that Cargo is installed
 cargo --version
