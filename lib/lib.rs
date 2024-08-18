dc039909fbae09b4ee138068b92c90b70c47f154
Prince Muel <vansomecsam@gmail.com>
2024-08-19 00:56:12 +0100

    feat: add to version control
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- lib/lib.rs
+++ lib/lib.rs
@@ -2,9 +2,17 @@ use prisma_client_rust::NewClientError;
 use serde::Serialize;
 use vercel_runtime::{Body, Error, Response, StatusCode};
 
+#[cfg(feature = "prisma_generated")]
 #[allow(warnings, unused)]
 pub mod prisma;
+
+#[cfg(feature = "prisma_generated")]
 use prisma::PrismaClient;
+
+#[cfg(feature = "prisma_generated")]
+async fn get_client() -> Result<PrismaClient, prisma_client_rust::NewClientError> {
+    prisma::new_client().await
+}
 
 pub fn method_not_allowed(
     val: impl Serialize,
