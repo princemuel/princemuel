c7bc30b09c6c73241c49a2f37e3c32bd1cf07a8c
Prince Muel <vansomecsam@gmail.com>
2024-08-20 05:20:34 +0100

    ci: migrate git hooks manager from husky to lefthook

--- .husky/install.mjs
+++
@@ -1,7 +0,0 @@
-import { env, exit } from "node:process";
-// Skip Husky install in production and CI
-if (env.NODE_ENV === "production" || env.CI === "true") exit(0);
-
-(async function () {
-  (await import("husky")).default();
-})();
