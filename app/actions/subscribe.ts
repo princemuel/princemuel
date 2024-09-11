e53b1df369311bf497ee9faf909dc03b5672387c
Prince Muel <68931805+princemuel@users.noreply.github.com>
2024-09-12 00:36:28 +0100

    feat: update subscribe actions & newsletter form

---
+++ app/actions/subscribe.ts
@@ -0,0 +1,40 @@
+import { resend } from "@/library/config/clients";
+import { envVars } from "@/library/config/environment";
+import { checkIfRateLimited } from "@/library/helpers/rate-limit";
+import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+export const subscribeAction = defineAction({
+  accept: "form",
+  input: z.object({
+    honeypot: z.string().max(0, "Invalid submission detected.").optional(),
+    email: z
+      .string({ message: "This field is required" })
+      .min(1, { message: "This field is required" })
+      .email({ message: "Please enter a valid email address" }),
+  }),
+  handler: async (body, { request }) => {
+    const { isRateLimited } = await checkIfRateLimited(request);
+
+    if (isRateLimited)
+      throw new ActionError({
+        code: "TOO_MANY_REQUESTS",
+        message: "You have reached your request limit",
+      });
+
+    const response = await resend.contacts.create({
+      email: body.email,
+      unsubscribed: false,
+      audienceId: envVars.RESEND_AUDIENCE,
+    });
+
+    return response.data
+      ? {
+          success: true,
+          payload: `Contact #${response.data.id.slice(0, 5)} created`,
+        }
+      : response.error
+        ? { success: false, payload: response.error.message }
+        : { success: false, payload: "Request failed" };
+  },
+});
