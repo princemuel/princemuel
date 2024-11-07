f1b9abf14e64f65c6208972e3b061bdcbe0a1f82
Prince Muel <vansomecsam@gmail.com>
2024-11-07 21:03:42 +0100

    chore: add preprender variabele

--- app/pages/blog/[slug]/og.png.ts
+++ app/pages/blog/[slug]/og.png.ts
@@ -1,14 +1,15 @@
-import { readFile } from "node:fs/promises";
-import { join, resolve } from "node:path";
-import { getCollection, getEntry } from "astro:content";
 import PlaceholderImage from "@/assets/images/blog-placeholder-5.jpg";
 import { handler } from "@/helpers/api-handler";
+import { getCollection, getEntry } from "astro:content";
+import { readFile } from "node:fs/promises";
+import { join, resolve } from "node:path";
 import satori from "satori";
 import { html } from "satori-html";
 import sharp from "sharp";
 
 import type { GetStaticPaths, InferGetStaticPropsType } from "astro";
 
+export const prerender = true;
 export const getStaticPaths = (async () => {
   const entries = await getCollection(
     "posts",
